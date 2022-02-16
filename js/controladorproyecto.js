dda1945406ba615a8bbc727d9c903e249531327d
203 <203@academicos.cesde.edu.co>
2022-02-16 10:04:41 -0500

    Se crea controlador

---
+++ js/controladorproyecto.js
@@ -0,0 +1,30 @@
+//Entradas del problema 
+
+
+let nombreProducto="camisa_dipper"
+let precioProducto=30000
+let pesoProducto=18.5
+let tieneDescuento=true
+let descripcionProducto="Camisa con el personaje de Dipper"
+let telefonoVendedor="3221542121"
+
+const NOMBRE_CLIENTE="Clara"
+
+
+//Arreglos unidimensionales 
+//Sirven para almacenar varios elementos en una sola variable 
+let productos=["camisa Dipper", "camisa Mabel", "gorra Stan", "cuaderno"]
+let productos2=Array("camisa Dipper", "camisa Mabel", "gorra Stan", "cuaderno")
+
+
+
+
+//Salidas del problema 
+
+
+console.log(nombreProducto)
+console.log("Su nombre es: "+NOMBRE_CLIENTE+" y su producto es: "+nombreProducto)
+console.log(`Su nombre es ${NOMBRE_CLIENTE} y su producto es ${nombreProducto}`)
+console.log("***************")
+console.log(productos)
+console.log(productos2[0])
